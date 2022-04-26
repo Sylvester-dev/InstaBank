63aeea0ef679c350ed8cfbb0179d418be89bea3e
Gautam Patodi <gpatodi15@gmail.com>
2022-04-26 17:28:02 +0530

    about page added

---
+++ frontend/src/components/About.js
@@ -0,0 +1,276 @@
+import React from "react";
+import { Button,CardText } from "reactstrap";
+
+
+export default function About() {
+  return (
+    <>
+      <div alt="mission" style={{ marginTop: "50px" }}>
+        <h1
+          style={{
+            fontSize: "30px",
+            margin: "20px",
+            display: "inline-block",
+            borderBottom: "3px solid",
+          }}
+        >
+          Our Mission Statement
+        </h1>
+        <p style={{ fontSize: "20px", margin: "20px", color: "#346194" }}>
+          InstaSafe is a peer to peer insurance system that runs on top of
+          ethereum blockchain.The platform is designed to eliminate the middle
+          agent in the insurance system and shorten the verification time to
+          almost zero.Our system also enables users to stream their money at a
+          fixed rate thats way both the user and the platform are in control of
+          the money and nobody loses ,also the premium is dynamic according to
+          the location for fair treatment of the system
+        </p>
+      </div>
+
+      <div alt="mission">
+        <h1
+          style={{
+            fontSize: "30px",
+            margin: "20px",
+            display: "inline-block",
+            borderBottom: "3px solid",
+          }}
+        >
+          Our Team
+        </h1>
+        <p style={{ fontSize: "20px", margin: "20px", color: "#346194" }}>
+          We’re proud to be a diverse team united by values including openness,
+          positivity, and drive that stems from our developers. No matter how
+          much our teams grow, we still feel like family.We understand that no
+          two ministries are the same, no two workers are identical, and we
+          strive to understand your unique needs and provide the best possible
+          benefits solutions. Our leadership team is comprised of caring,
+          educated and experienced professionals who face each decision with one
+          question in mind, “What is the best solution for those we serve?” At
+          every level of the organization you will find compassionate,
+          knowledgeable team members ready to meet your needs and answer your
+          questions. Taking care of your benefits isn’t just a job, it is our
+          passion to help those in His service.
+        </p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#ffffff",
+          marginTop: "100px",
+        }}
+      >
+        <div
+          style={{
+            width: "400px",
+            height: "500px",
+            background: "#346194",
+            margin: "auto",
+            borderRadius: "50px",
+          }}
+        >
+          <div style={{ marginTop: "20px" }}>
+            <img
+              alt="CardImage"
+              src="https://ipfs.io/ipfs/bafkreias7qu7jkleeahvm2xp5svc4d6kkwkcnbnnz4sxxudusxinbrldfm"
+              style={{
+                borderRadius: "10px",
+                height: "200px",
+                width: "200px",
+                marginLeft: "25%",
+                marginBottom: "20px",
+              }}
+            />
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Vineet Kumar
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              Hard choices, easy life. Easy choices, hard life.
+            </CardText>
+            <Button
+              style={{
+                alignItems: "center",
+                marginLeft: "40%",
+                marginRight: "auto",
+              }}
+              href="https://www.linkedin.com/in/vineet-kumar-a67792197/"
+            >
+              <img
+                alt="linkedin_prop"
+                src="https://img.icons8.com/material-outlined/50/000000/linkedin--v1.png"
+                style={{}}
+              />
+            </Button>
+          </div>
+        </div>
+
+        <div
+          style={{
+            width: "400px",
+            height: "500px",
+            background: "#346194",
+            margin: "auto",
+            borderRadius: "50px",
+          }}
+        >
+          <div style={{ marginTop: "20px" }}>
+            <img
+              alt="CardImage"
+              src="https://siasky.net/OACgFb5Uek4vOw-_eR_z-BRvyT9rjqk3OPdV6nvTAT8erQ"
+              style={{
+                borderRadius: "10px",
+                height: "200px",
+                width: "200px",
+                marginLeft: "25%",
+                marginBottom: "20px",
+              }}
+            />
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Hritwik Tripathi
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              Patience and hard work pay off, never doubt or lose faith in them.
+            </CardText>
+            <Button
+              style={{
+                alignItems: "center",
+                marginLeft: "40%",
+                marginRight: "auto",
+              }}
+              href="https://www.linkedin.com/in/hritwikt/"
+            >
+              <img
+                alt="linkedin_prop"
+                src="https://img.icons8.com/material-outlined/50/000000/linkedin--v1.png"
+                style={{ marginLeft: "0px" }}
+              />
+            </Button>
+          </div>
+        </div>
+
+        <div
+          style={{
+            width: "400px",
+            height: "500px",
+            background: "#346194",
+            margin: "auto",
+            borderRadius: "50px",
+          }}
+        >
+          <div style={{ marginTop: "20px" }}>
+            <img
+              alt="CardImage"
+              src="https://ipfs.io/ipfs/bafkreidrbxjca7o3x6dlcxica3b6e3qfkly5gyggzqburqsc3hnrespnou"
+              style={{
+                borderRadius: "10px",
+                height: "200px",
+                width: "200px",
+                marginLeft: "25%",
+                marginBottom: "20px",
+              }}
+            />
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Gautam Patodi
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              Hard work pays off if you’re patient enough to see it through.
+            </CardText>
+            <Button
+              style={{
+                alignItems: "center",
+                marginLeft: "40%",
+                marginRight: "auto",
+              }}
+              href="https://www.linkedin.com/in/gautam-patodi-7a6849195/"
+            >
+              <img
+                alt="linkedin_prop"
+                src="https://img.icons8.com/material-outlined/50/000000/linkedin--v1.png"
+                style={{}}
+              />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#ffffff",
+          marginTop: "50px",
+          marginBottom: "100px",
+        }}
+      >
+        <div
+          style={{
+            width: "400px",
+            height: "500px",
+            background: "#346194",
+            margin: "auto",
+            borderRadius: "50px",
+          }}
+        >
+          <div style={{ marginTop: "20px" }}>
+            <img
+              alt="CardImage"
+              src="https://ipfs.io/ipfs/bafkreidkwdurljvbinmvymf6ukdxm2y3udfjv5f7357k7lkqho5zxmfdfi"
+              style={{
+                borderRadius: "10px",
+                height: "200px",
+                width: "200px",
+                marginLeft: "25%",
+                marginBottom: "20px",
+              }}
+            />
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Mayank Verma
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              The harder you work for something, the greater you will feel when
+              you achieve it.
+            </CardText>
+            <Button
+              style={{
+                alignItems: "center",
+                marginLeft: "40%",
+                marginRight: "auto",
+              }}
+              href="https://www.linkedin.com/in/mayank-verma-61444a18b/"
+            >
+              <img
+                alt="linkedin_prop"
+                src="https://img.icons8.com/material-outlined/50/000000/linkedin--v1.png"
+                style={{}}
+              />
+            </Button>
+          </div>
+        </div>
+
+        <div
+          style={{
+            width: "400px",
+            height: "500px",
+            background: "#346194",
+            margin: "auto",
+            borderRadius: "50px",
+          }}
+        >
+        </div>
+      </div>
+
+      <p style={{ marginLeft: "45%" }}>Made with ❤️ by Team InstaBank</p>
+    </>
+  );
+}
