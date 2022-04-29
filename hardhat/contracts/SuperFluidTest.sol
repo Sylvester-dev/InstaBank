5079324fdcacfb301890fac6a0ff45941ccd6365
Vineet Kumar <56035194+Sylvester-dev@users.noreply.github.com>
2022-04-29 14:08:44 +0530

    Creating user and transferring dai

--- hardhat/contracts/SuperFluidTest.sol
+++ hardhat/contracts/SuperFluidTest.sol
@@ -81,3 +81,93 @@ contract SuperFluidTest is SuperAppBase {
       emit UserRegistered(msg.sender);
     }
   }
+
+  function transferFDaiToAddress(address receiver, uint256 amount) external {
+    // Check supertoken balance
+    uint256 contractBalance =
+      _superfluid_accpeted_token.balanceOf(address(this));
+    if (amount > contractBalance) {
+      revert("Insufficient SuperToken balance");
+    }
+
+    // Check underlying token balance
+    _superfluid_accpeted_token.downgrade(amount);
+    IERC20 underlyingToken =
+      IERC20(_superfluid_accpeted_token.getUnderlyingToken());
+    uint256 contractUnderlyingBalance =
+      underlyingToken.balanceOf(address(this));
+    if (contractUnderlyingBalance < amount) {
+      revert("Insufficient Base Token Balance");
+    }
+
+    // Do the transfer
+    underlyingToken.transfer(receiver, amount);
+    emit UnderlyingTokenTransferred(receiver, amount);
+  }
+
+  // Superfluid Functions
+  modifier onlySuperfluidHost() {
+    require(
+      msg.sender == address(_superfluid_host),
+      "onlySuperfluidHost: callback called from wrong sender"
+    );
+    _;
+  }
+
+  function beforeAgreementCreated(
+    ISuperToken, /* superToken */
+    address, /* agreementClass */
+    bytes32, /*agreementId*/
+    bytes calldata, /*agreementData*/
+    bytes calldata /*ctx*/
+  ) external view override onlySuperfluidHost returns (bytes memory cbdata) {
+    cbdata = bytes("");
+  }
+
+  function afterAgreementCreated(
+    ISuperToken, /* superToken */
+    address, /*agreementClass*/
+    bytes32, /* agreementId*/
+    bytes calldata, /*agreementData*/
+    bytes calldata, /*cbdata*/
+    bytes calldata ctx
+  ) external override onlySuperfluidHost returns (bytes memory newCtx) {
+    address sender = _superfluid_host.decodeCtx(ctx).msgSender;
+
+    isAddressSendingPremiums[sender] = true;
+    addressesSendingPremiumsCount += 1;
+    console.log(
+      "afterAgreementCreated: Agreement created by address: ",
+      sender
+    );
+    return ctx;
+  }
+
+  function beforeAgreementTerminated(
+    ISuperToken, /*superToken*/
+    address, /* agreementClass */
+    bytes32, /*agreementId*/
+    bytes calldata, /*agreementData*/
+    bytes calldata /*ctx*/
+  ) external view override onlySuperfluidHost returns (bytes memory cbdata) {
+    // According to the app basic law, we should never revert in a termination callback
+    return bytes("");
+  }
+
+  function afterAgreementTerminated(
+    ISuperToken, /* superToken */
+    address, /* agreementClass */
+    bytes32, /* agreementId */
+    bytes calldata agreementData,
+    bytes calldata, /* cbdata */
+    bytes calldata ctx
+  ) external override onlySuperfluidHost returns (bytes memory newCtx) {
+    // note that msgSender can be either flow sender, receiver or liquidator
+    // one must decode agreementData to determine who is the actual player
+    (address user, ) = abi.decode(agreementData, (address, address));
+    isAddressSendingPremiums[user] = false;
+    addressesSendingPremiumsCount -= 1;
+
+    return ctx;
+  }
+}
